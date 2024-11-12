bb0e0d87530268e04e627668170b2ef5a328e6e3
Jie Wu <wujie3375@gmail.com>
2024-11-12 20:21:40 +0800

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -1904,3 +1904,4 @@ $(document).ready(function() {
 function scrollToTop() {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+document.getElementById("rocket").addEventListener("click", scrollToTop);
